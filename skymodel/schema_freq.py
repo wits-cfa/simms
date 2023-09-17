31b4f0f266eed923b5c3dac7349d054acdcd71a9
Shibre Sete <117459156+Shibre@users.noreply.github.com>
2023-09-17 14:09:52 +0200

    Updated_schema_freq.py
    'The List[float] str not supported' error turns out to be a mapping problem. so I changed 'List' - list to 'List[float] - list, 'from the mapping.
    Then, when you try to validate, a ''key': Line' error arises. I have read that this will be solved by defining a schema dictionary that contains the 'Line' and 'Cont'. So I defined a schema dictionary to solve the key error.
    
    the code works for me. check it out and let me know.
    And we might not need a  Regex.
    
    (there are some more changes too)

--- skymodel/schema_freq.py
+++ skymodel/schema_freq.py
@@ -1,120 +1,99 @@
+
 from dataclasses import dataclass
 from typing import Any, List, Dict, Optional, Union
 from enum import Enum
 import yaml
-from  utilities import ValidationError, MyList
+#import schema
+#from validator import validate
 
-SCHEMA = "./schema_freq.yaml"
+
+SCHEMA = "./schema.yaml"
+
+TYPES = [int, float, bool, str, list, Union]
 
 TYPE_MAP = {
     'int': int,
     'float': float,
     'bool': bool,
     'str': str,
-    "List": MyList,
-    #'Union': Union,
+    'List[float]': list,
+    'Union': Union
     }
 
-def type_map(dtype):
-    """
-    Ensures that the data type is a data type and not a string.
-    """
-    if isinstance (dtype, str):
-        if dtype.startswith('List['):
-            return MyList
-        else:
-            return TYPE_MAP.get(dtype, None)
-    else:
-        return dtype
-    
+
+class ValidationError(Exception):
+    pass
 
 
 class Parameter(object):
     def __init__(self, dtype, info, default=(), required=False):
-        self.dtype = type_map(dtype)
-        self.info = info
+        self.dtype = TYPE_MAP.get(dtype, dtype)
+        self.info = TYPE_MAP.get(info, info)
         self.default = default
-        self.required = required
+        self.required = TYPE_MAP.get(required, required)
 
+        if self.dtype not in TYPES:
+            raise ValidationError(f"Type {dtype} {dtype.__class__.__name__} is not supported.")
+    
+        if default and not isinstance(default, dtype):
+            raise ValidationError(f"Default value does not match dtype ({dtype.__class__.__name__})")
 
-        
-        if self.dtype not in TYPE_MAP.values():
-            raise ValidationError(f"Type {self.dtype} is not supported.")
-
-
-        if self.default and not self.__isinstance(self.default, self.dtype):
-            raise ValidationError(f"Default value does not match dtype ({self.dtype.__class__.__name__})")
-
-        if not isinstance(self.required, bool):
+        if not isinstance(required, bool):
             raise ValidationError("The required option has to be a boolean")
     
-    def __isinstance(self, value, dtype):
-        """
-        Extends the isinstance() function to handle our MyList types
-
-        Parameters
-        ---------
-        value: Any
-            The value being checked
-        dtype:
-            The type that value has to be
-
-        
-        Returns
-        --------------
-        Boolean value indicating whether value is of type dtype.
-        """
-        if dtype is MyList:
-            """
-            Need to figure out how to validate list of stuff
-            """
-        else:
-            return isinstance(value, dtype)
-    
     def setme(self, value):
+        if self.dtype == List[float]:
+            self.dtype = List[float]
+        #if isinstance(value, list) and all(isinstance(item, float) for item in value):
         if isinstance(value, self.dtype):
             self.value = value
         else:
-            raise ValidationError( f"Parameter has wrong type. "
-                                  f"Expected type: {self.dtype.__name__}, "
-                                  f"Actual type: {type(value).__name__}, "
-                                  f"Actual value: {value}")
-    
-def validate(valme, schemafile=SCHEMA):
-    with open(schemafile) as stdr:
-        schema = yaml.load(stdr, Loader=yaml.FullLoader)
+            raise ValidationError( f"Parameter has wrong type. Expected type: {self.dtype.__name__}, "f"Actual type: {type(value).__name__}, Actual value: {value}")
 
-    section = schema[valme.__class__.__name__]
-    for key in section:
-        if isinstance(section[key], str):
-            continue 
-        param = Parameter(**section[key])
-        value = getattr(valme, key, None)
-        print(f"Processing key: {key}, value: {value}")
-        if value is None and param.required:
-            raise ValidationError(f"Required parameter '{key}' has not been set")
-        param.setme(value)
+schema = {
+    'Line': {
+        'freq_peak': {'dtype': 'float', 'info': 'float', 'default': None, 'required': True},
+        'width': {'dtype': 'int', 'info': 'int', 'default': None, 'required': True},
+        'stokes': {'dtype': 'float', 'info': 'float', 'default': None, 'required': True}
+    },
+    'Cont': {
+        'ref_freq': {'dtype': 'float', 'info': 'float', 'default': None, 'required': True},
+        'stokes': {'dtype': 'float', 'info': 'float', 'default': None, 'required': True},
+        'coeffs': {'dtype': 'List[float]', 'info': 'List[float]', 'default': None, 'required': True}
+    }
+}
 
 @dataclass
 class Line(object):
-    """
-    blah blah
-    """
     freq_peak: float
     width: int
     stokes: float
 
 @dataclass
 class Cont(object):
-    """
-    blah blah
-    """
     ref_freq: float
     stokes: float
     coeffs: List[float]
 
+def validate(valme, schema):
+    class_name = valme.__class__.__name__
+    section = schema.get(class_name)
+
+    if section is None:
+        raise ValidationError(f"No schema found for class '{class_name}'")
+
+    for key, value in section.items():
+        if isinstance(value, str):
+            continue
+        param = Parameter(**value)
+        attr_value = getattr(valme, key, None)
+        print(f"Processing key: {key}, value: {attr_value}")
+        param.setme(attr_value)
 
 line = Line(1.3, 4, 10.2)
-validate(line)
-cont=Cont(5.2, 10.6, [5.1,2.2,3.4,5])
-validate(cont)
+validate(line, schema)
+
+cont = Cont(5.2, 10.6, ['hey'])
+validate(cont, schema)
+
+
