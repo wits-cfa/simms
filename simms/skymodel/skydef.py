295d49a9158556fedfa7b089760f9b1b278c2551
sphemakh <sphemakh@gmail.com>
2023-10-06 14:48:55 +0200

    Fixes #12

--- simms/skymodel/skydef.py
+++ simms/skymodel/skydef.py
@@ -1,8 +1,8 @@
 from dataclasses import dataclass
-from typing import List
+from typing import List, Optional
 from simms import SCHEMADIR
 import os
-from simms.utilities import readyaml
+from simms.utilities import readyaml, get_class_attributes, ValidationError
 
 class Base(object):
     def __init__(self, schemafile=None):
@@ -16,19 +16,39 @@ class Base(object):
         else:
             raise FileNotFoundError(f"Schema file '{schemafile}' could not be found")
 
+    def validate_section(self, section=None):
+        section = section or self.schema_section
+        class_set = set(get_class_attributes(self))
+        section_set = set(self.schema[section].keys())
+
+        # ignore these parameters
+        novalidate = set(["schemafile", "schema_section", "schema"])
+        class_set_valid = class_set.difference(novalidate)
+        # check for schema/class mismatches with the rest of the parameters
+        mismatch = class_set_valid.difference(section_set)
+        if mismatch:
+            raise ValidationError(f"Schema file, {self.schemafile}"
+                                  f", does not match class definition"
+                                  f" for section: {section}."
+                                  f"Mismatched parameters are: {mismatch}")
+
+        return True
+        
+
 @dataclass
 class Line(Base):
     freq_peak: float
     width: int
-    stokes: float
+    stokes: List[float]
+    restfreq: Optional[float] = None
     schemafile: str = os.path.join(SCHEMADIR, "schema_freq.yaml")
     schema_section: str = "Line"
 
-
+    
 @dataclass
 class Cont(Base):
     ref_freq: float
-    stokes: float
-    coeffs: List[float]
+    stokes: List[float]
+    coeffs: Optional[List[float]] = None
     schema_section: str = "Cont"
     schemafile: str = os.path.join(SCHEMADIR, "schema_freq.yaml")
--- tests/skyspec_test.py
+++ tests/skyspec_test.py
@@ -1,7 +1,21 @@
 from simms.skymodel.skydef import Line, Cont
 from simms.config_spec import validate
 
+print("Testing base API")
+line = Line(1.3, 4, 10.2, restfreq=1.42e9)
+validate(line)
+print()
+
+print("Test for case where optional parameters are not set")
 line = Line(1.3, 4, 10.2)
 validate(line)
+print()
+
+print("Test API with a list value")
 cont = Cont(5.2, 10.6, [5.1,2.2,3.4,1])
+validate(cont)
+print()
+
+print("Test API with default value not set")
+cont = Cont(5.2, 10.6)
 validate(cont)
