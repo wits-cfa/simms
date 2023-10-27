911a0015be6613e9e2da2053ef2597e8d1a9c1f3
MukundiRams <2094318@students.wits.ac.za>
2023-10-27 17:18:10 +0200

    deleting the layout files

--- simms/telescope/teldef.py
+++ simms/telescope/teldef.py
@@ -4,12 +4,17 @@ from simms import SCHEMADIR
 from simms.utilities import File
 import os
 from simms.config_spec import SpecBase
+from simms.telescope import array_utilities
+from casacore.measures import measures
+import numpy as np
+import ephem
+
 
 @dataclass
 class Antenna(SpecBase):
     name: str
     mount: str = "ALT-AZ"
-    size: float = 13.5
+    size: float = None
     sefd: float = None
     tsys_over_eta: float = 0 # Zero is unset
     sensitivity_file: File = None
@@ -36,12 +41,49 @@ class Observation(SpecBase):
     telescope: Any
     direction: List[str]
     start_time: str
-    dtime: float = 10
-    ntimes: int = 10 
-    start_freq: Union[str,float] = "900MHz"
-    dfreq: Union[str,float] = "2MHz"
-    nchan: int = 10
-    correlations: List[str] = ["XX", "YY"]
+    dtime: float = None
+    ntimes: int = None 
+    start_freq: Union[str,float] = None
+    dfreq: Union[str,float] = None
+    nchan: int = None
+    correlations: List[str] = None
     schemafile: str = os.path.join(SCHEMADIR, "schema_observation.yaml")
     schema_section: str = "Observation"
     
+    
+    def get_uvw(self,h0,longitude,latitude,data_file = None,observatory=None):
+        """
+        Get the uvw baseline positions using the global2uvw function in Array class
+        """
+        pointing_direction = self.direction
+        observatory = self.telescope or observatory
+
+        initialization = array_utilities.Array(pointing_direction=pointing_direction,
+                                              data_file=data_file,
+                                              observatory=observatory)
+        
+        uvw,time = initialization.global2uvw(h0=h0,longitude = longitude,
+                                                  latitude = latitude,
+                                                  date = self.start_time,
+                                                  dtime = self.dtime,
+                                                  ntimes = self.ntimes)
+        
+        return uvw,time
+
+  
+
+
+
+obj = Observation(name = 'test',
+                  desc = 'testing',
+                  telescope='meerkat',
+                  direction = ['J200','0deg','-30deg'],
+                  start_time=['UTC','2023/10/25 12:0:0'],
+                  dtime = 10,
+                  ntimes = 10
+                  )
+obj_uvw = obj.get_uvw(h0 = [-1,1],
+            longitude = 21.440810448238672,
+                   latitude = -30.714037075541743,
+                   )
+print(obj_uvw[0].shape)
