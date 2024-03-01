dfb48fd67326040fb7aaa526f29a96196148f960
Allycan <allymap@gmail.com>
2024-03-01 13:33:45 +0200

    fixed conflicts in config_spec.py

--- simms/constants.py
+++ simms/constants.py
@@ -1,5 +1,17 @@
+# Frequently used constants are defined here
 import numpy as np
 
 PI = np.pi
 C = 2.99792458e8
+<<<<<<< HEAD
 FWHM = 2* np.sqrt(2 * np.log(2)) #fwhm constant 
+=======
+# fwhm constant 
+FWHM_E = np.sqrt(2 * np.log(2))
+#Earth's semi major axis.
+earth_emaj = 6378137. #[m]
+#Earth's first numerical eccentricity
+esq = 0.00669437999014
+
+hour_angle = PI/12
+>>>>>>> frequency_axis_1
---
+++ simms/telescope/layouts/__init__.py
@@ -0,0 +1,30 @@
+from typing import Dict
+import glob
+import os
+
+#workaround the issue stated in
+#  https://github.com/python/mypy/issues/1422
+__path__ = os.path.dirname(__file__)
+
+def get_layout(name):
+    """
+    Get the specified layout information.
+
+    """
+    fname = os.path.join(__path__, f"{name}.geodetic.yaml")
+    if os.path.exists(fname):
+        return fname
+    raise FileNotFoundError("Layout not part of our known layouts")
+
+def known()-> Dict:
+    """
+    Returns a dictionary of known array layouts
+    """
+    lays = glob.glob(f"{__path__}/*.geodetic.yaml")
+    laysdict = {}
+    for layout in lays:
+        basename = os.path.basename(layout)
+        lname = basename.split(".geodetic.yaml")[0]
+        laysdict[lname] = layout
+    
+    return laysdict
