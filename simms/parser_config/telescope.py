baefbb46e1699a9b4107790c96c00fcd69aadeb0
Allycan <allymap@gmail.com>
2024-03-05 17:10:43 +0200

    amended the parser_config files for telescope

--- simms/parser_config/telescope.py
+++ simms/parser_config/telescope.py
@@ -1,11 +1,24 @@
-import simms
-from simms.parser_config.utils import load
-from scabha.schema_utils import clickify_parameters
+import glob
+import os
+
 import click
 from omegaconf import OmegaConf
+from scabha.schema_utils import clickify_parameters, paramfile_loader
+
+import simms
+from simms.parser_config.utils import load
+
+# from scabha.basetypes import File
+from simms.utilities import File
 
 command = "telescope"
 config = load(command)
+
+thisdir = os.path.dirname(__file__)
+telescope_files = glob.glob(f"{thisdir}/library/*.yaml")
+telefiles = [File(item) for item in telescope_files]
+parserfile = File(f"{thisdir}/{command}.yaml")
+config = paramfile_loader(parserfile, telefiles)[command]
 
 
 @click.command(command)
@@ -14,5 +27,6 @@ config = load(command)
 def runit(**kwargs):
     opts = OmegaConf.create(kwargs)
     print(opts)
-    
+
+
 runit()
