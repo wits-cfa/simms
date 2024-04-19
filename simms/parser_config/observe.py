6f7c4e95bd444eacf980714166ba5a1905675d79
Allycan <allymap@gmail.com>
2024-04-19 16:03:41 +0200

    fixed the transformation matrixs for array utlities module

--- simms/parser_config/observe.py
+++ simms/parser_config/observe.py
@@ -1,8 +1,9 @@
-import simms
-from simms.parser_config.utils import load, load_sources
-from scabha.schema_utils import clickify_parameters
 import click
 from omegaconf import OmegaConf
+from scabha.schema_utils import clickify_parameters
+
+import simms
+from simms.parser_config.utils import load, load_sources
 
 command = "observe"
 sources = load_sources(["skysim", "telescope"])
@@ -15,7 +16,32 @@ config = load(command, use_sources=sources)
 def runit(**kwargs):
     opts = OmegaConf.create(kwargs)
     print(opts)
-    
-runit()
 
-  
+
+"""import glob
+import os
+
+import click
+from omegaconf import OmegaConf
+from scabha.basetypes import File
+from scabha.schema_utils import clickify_parameters, paramfile_loader
+
+import simms
+from simms.parser_config.utils import load
+
+command = "observe"
+config = load(command)
+
+thisdir = os.path.dirname(__file__)
+observe_params = glob.glob(f"{thisdir}/library/*.yaml")
+observe_files = [File(item) for item in observe_params]
+parserfile = File(f"{thisdir}/{command}.yaml")
+config = paramfile_loader(parserfile, observe_files)[command]
+
+
+@click.command(command)
+@click.version_option(str(simms.__version__))
+@clickify_parameters(config)
+def runit(**kwargs):
+    opts = OmegaConf.create(kwargs)
+    print(opts)"""
--- simms/parser_config/telescope.py
+++ simms/parser_config/telescope.py
@@ -3,22 +3,20 @@ import os
 
 import click
 from omegaconf import OmegaConf
+from scabha.basetypes import File
 from scabha.schema_utils import clickify_parameters, paramfile_loader
 
 import simms
 from simms.parser_config.utils import load
 
-# from scabha.basetypes import File
-from simms.utilities import File
-
 command = "telescope"
 config = load(command)
 
 thisdir = os.path.dirname(__file__)
-telescope_files = glob.glob(f"{thisdir}/library/*.yaml")
-telefiles = [File(item) for item in telescope_files]
+telescope_params = glob.glob(f"{thisdir}/library/*.yaml")
+telescope_files = [File(item) for item in telescope_params]
 parserfile = File(f"{thisdir}/{command}.yaml")
-config = paramfile_loader(parserfile, telefiles)[command]
+config = paramfile_loader(parserfile, telescope_files)[command]
 
 
 @click.command(command)
@@ -27,6 +25,3 @@ config = paramfile_loader(parserfile, telefiles)[command]
 def runit(**kwargs):
     opts = OmegaConf.create(kwargs)
     print(opts)
-
-
-runit()
