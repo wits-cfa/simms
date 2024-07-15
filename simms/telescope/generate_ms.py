fb6141a88ceafbc92a5e90d75565fdc6bcee9c97
MukundiRams <2094318@students.wits.ac.za>
2024-07-15 17:45:59 +0000

    added missing columns of the ms

--- simms/telescope/generate_ms.py
+++ simms/telescope/generate_ms.py
@@ -8,7 +8,7 @@ import daskms
 import numpy as np
 from casacore.measures import measures
 from casacore.tables import table
-from daskms import xds_to_table
+from daskms import xds_to_table,xds_from_table
 from scabha.basetypes import File
 from simms.telescope import array_utilities as autils
 from omegaconf import OmegaConf
@@ -78,6 +78,7 @@ def create_ms(ms_name: str, telescope_name: Union[str, File],
     times = da.from_array(uvcoverage_data.times, chunks=num_row_chunks)
     time_range = np.array(
         [[uvcoverage_data.times[0], uvcoverage_data.times[-1]]])
+    duration = ntimes * dtime
     uvw = da.from_array(uvcoverage_data.uvw, chunks=(num_row_chunks, 3))
     antenna1 = da.from_array(ant1, chunks=num_row_chunks)
     antenna2 = da.from_array(ant2, chunks=num_row_chunks)
@@ -85,8 +86,11 @@ def create_ms(ms_name: str, telescope_name: Union[str, File],
     interval = da.rechunk(interval, chunks=num_row_chunks)
     sigma = da.full((num_rows, num_corr), 1.)
     sigma = da.rechunk(sigma, chunks=(num_row_chunks, num_corr))
+    sigma_spec = da.full((num_rows, num_chans,num_corr), 1.)
+    sigma_spec = da.rechunk(sigma_spec, chunks=(num_row_chunks, num_chans,num_corr))
     flag = da.zeros((num_rows, num_chans, num_corr),
                     dtype=bool, chunks=(num_row_chunks, num_chans, num_corr))
+    scan_number = da.rechunk(da.full(num_rows,1),chunks=num_row_chunks)
 
     freqs = uvcoverage_data.freqs
     freqs = freqs.reshape(1, freqs.shape[0])
@@ -94,6 +98,7 @@ def create_ms(ms_name: str, telescope_name: Union[str, File],
     chan_width = dm.frequency(v0=dfreq)["m0"]["value"]
     channel_widths = np.full(freqs.shape, chan_width)
     total_bandwidth = nchan * chan_width
+
 
     noise = sefd / np.sqrt(abs(2*chan_width*dtime))
     dummy_data = np.random.randn(num_rows, num_chans, num_corr) + \
@@ -114,9 +119,12 @@ def create_ms(ms_name: str, telescope_name: Union[str, File],
         "ANTENNA2": (("row"), antenna2),
         "SIGMA": (("row", "corr"), sigma),
         "WEIGHT": (("row", "corr"), sigma),
-        "SIGMA_SPECTRUM": (("row", "corr"), sigma),
-        "WEIGHT_SPECTRUM": (("row", "corr"), sigma),
-        "FLAG": (("row", "chan", "corr"), flag)
+        "SIGMA_SPECTRUM": (("row", "chan","corr"), sigma_spec),
+        "WEIGHT_SPECTRUM": (("row","chan", "corr"), sigma_spec),
+        "FLAG": (("row", "chan", "corr"), flag),
+        "FLAG_CATEGORY":(("row","flagcat","chan","corr"),flag[:,None,:,:]),
+        "SCAN_NUMBER":(("row"),scan_number)
+        
     }
 
     if addnoise:
@@ -127,6 +135,8 @@ def create_ms(ms_name: str, telescope_name: Union[str, File],
 
     write_main = xds_to_table(main_table, f"{ms_name}.ms")
     dask.compute(write_main)
+    
+
 
     spw_tab = table(f"{ms_name}.ms::SPECTRAL_WINDOW",
                     readonly=False, lockoptions='user', ack=False)
@@ -144,9 +154,11 @@ def create_ms(ms_name: str, telescope_name: Union[str, File],
         spw_tab.putcol("NUM_CHAN", nchan)
         spw_tab.putcol("NAME", "00")
         spw_tab.putcol("NET_SIDEBAND", [1])
+        spw_tab.putcol("FREQ_GROUP_NAME","Group 1")
     finally:
         spw_tab.unlock()
         spw_tab.close()
+
 
     dish_diameter = [size] * num_ants
     ant_mount = [mount] * num_ants
@@ -201,6 +213,7 @@ def create_ms(ms_name: str, telescope_name: Union[str, File],
         obs_tab.addrows(1)
         obs_tab.putcol("TIME_RANGE", time_range)
         obs_tab.putcol("OBSERVER", 'simms simulator')
+        obs_tab.putcol("PROJECT",'simms simulation')
         obs_tab.putcol("TELESCOPE_NAME", telescope_name)
     finally:
         obs_tab.unlock()
@@ -210,7 +223,14 @@ def create_ms(ms_name: str, telescope_name: Union[str, File],
                       readonly=False, lockoptions='user', ack=False)
     try:
         pntng_tab.lock(write=True)
-        pntng_tab.addrows(1)
+        pntng_tab.addrows(num_rows)
+        pntng_tab.putcol("TARGET",np.full((num_rows,1,2),phase_dir))
+        pntng_tab.putcol("DIRECTION",np.full((num_rows,1,2),phase_dir))
+        pntng_tab.putcol("TIME",uvcoverage_data.times)
+        pntng_tab.putcol("INTERVAL",np.full(num_rows,dtime))
+        pntng_tab.putcol("TRACKING",np.full(num_rows,True))
+        
+    
     finally:
         pntng_tab.unlock()
         pntng_tab.close()
@@ -230,8 +250,19 @@ def create_ms(ms_name: str, telescope_name: Union[str, File],
     feed_tab = table(f"{ms_name}.ms::FEED",
                      readonly=False, lockoptions='user', ack=False)
     try:
+        pol_response = np.array([[[1.+0.j, 0.+0.j],
+        [0.+0.j, 1.+0.j]]])
         feed_tab.lock(write=True)
         feed_tab.addrows(num_ants)
+        feed_tab.putcol("ANTENNA_ID",np.arange(num_ants))
+        feed_tab.putcol("BEAM_ID",np.full(num_ants,-1))
+        feed_tab.putcol("BEAM_OFFSET",np.zeros((num_ants,2,2),dtype=float))
+        feed_tab.putcol("INTERVAL",np.full(num_ants,1.e30))
+        feed_tab.putcol("NUM_RECEPTORS",np.full(num_ants,2))
+        feed_tab.putcol("SPECTRAL_WINDOW_ID",np.full(num_ants,-1))
+        feed_tab.putcol("POLARIZATION_TYPE",np.full((num_ants,2),(['X','Y'])))
+        feed_tab.putcol("POL_RESPONSE",np.full((num_ants,2,2),pol_response))
     finally:
         feed_tab.unlock()
         feed_tab.close()
+    
