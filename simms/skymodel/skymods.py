fix beam scaling issues

simms/skymodel/skymods.py
@@ -194,7 +194,7 @@ def skymodel_from_fits(input_fitsimages: Union[File, List[File]], ra0: float, de
                             "Cannot interpolate FITS image onto MS frequency grid.")
 
 
-    trgt_shape = ["RA", "DEC", "FREQ", "STOKES"]
+    trgt_shape = ["STOKES", "RA", "DEC", "FREQ"]
     skymodel = fds.get_xds(transpose=trgt_shape).data
     
     fds.close()
@@ -209,8 +209,8 @@ def skymodel_from_fits(input_fitsimages: Union[File, List[File]], ra0: float, de
             bmaj = fds.beam_table["BMAJ"].to("rad").value
             bmin = fds.beam_table["BMIN"].to("rad").value
             beam_area = (np.pi * bmaj * bmin) / (4 * np.log(2)) #this should also be an array
-            beam_area_pixels = beam_area / pixel_area
-            skymodel = skymodel / beam_area_pixels[np.newaxis, np.newaxis, :, np.newaxis]
+            pixels_per_beam = beam_area / pixel_area 
+            skymodel = skymodel / pixels_per_beam[np.newaxis, np.newaxis, np.newaxis, :]
             
             #check only the the value of the 
         else:
@@ -227,10 +227,10 @@ def skymodel_from_fits(input_fitsimages: Union[File, List[File]], ra0: float, de
         if nchan != nchan_fits or np.any(chan_freqs != fits_freqs):
             log.warning("Interpolating FITS sky model onto MS frequency grid.  This can use a lot of memory.")
             interp_stokes = []
-            for stokes in range(skymodel.shape[-1]):
+            for stokes in range(fds.coords["STOKES"].size):
                 
                 data = xr.DataArray(
-                    skymodel[..., stokes],
+                    skymodel[stokes,...],
                     coords={"ra": ra_grid, "dec": dec_grid, "freq": fits_freqs},
                     dims=["ra", "dec", "freq"]
                 )
@@ -243,18 +243,19 @@ def skymodel_from_fits(input_fitsimages: Union[File, List[File]], ra0: float, de
                 interp_stokes.append(interp_data.data)
 
             # combine into new array with shape (n_stokes, n_pix_l, n_pix_m, len(chan_freqs))
-            skymodel = da.stack(interp_stokes, axis=3)
+            skymodel = da.stack(interp_stokes, axis=0)
     else:
         expand_freq_dim = nchan > 1
     
-    skymodel = StokesDataFits(fds.coords["STOKES"], dim_idx=-1, data=skymodel)
+    skymodel = StokesDataFits(fds.coords["STOKES"], dim_idx=0, data=skymodel)
     # The stokes parameters in this class will be transposed to the correct basis.
     
     predict_image = skymodel.get_brightness_matrix(ncorr, linear_pol_basis = basis == "linear")
-     
-    
-    # reshape predict_image to im_to_vis expectations
-    reshaped_predict_image = predict_image.reshape(n_pix_l * n_pix_m, 1 if expand_freq_dim else nchan, ncorr)
+    predict_nchan = 1 if expand_freq_dim else nchan
+    # first transpose stokes axis to the end,
+    predict_image = np.transpose(predict_image, (1,2,3,0))
+    # then reshape predict_image to im_to_vis expectations
+    reshaped_predict_image = predict_image.reshape(n_pix_l * n_pix_m, predict_nchan, ncorr)
     
     # get only pixels with brightness > tol
     tol_mask = np.any(np.abs(reshaped_predict_image) > tol, axis=(1, 2))

simms_tests/fits_processing_tests.py
@@ -7,7 +7,6 @@ from astropy.io import fits
 from astropy.wcs import WCS
 from astropy.table import Table
 from simms import BIN, get_logger
-from simms.skymodel.converters import convert2rad
 from simms.skymodel.skymods import skymodel_from_fits
 from simms.utilities import FITSSkymodelError as SkymodelError
 
@@ -537,11 +536,11 @@ class TestFITSProcessing(unittest.TestCase):
         
         header = wcs.to_header()
         header['BUNIT'] = 'Jy/beam'
-        header['BMAJ'] = 0.01  # degrees
-        header['BMIN'] = 0.005 # degrees
+        header['BMAJ'] = self.cell_size * 7 /3600 # degrees
+        header['BMIN'] = self.cell_size* 5 / 3600 # degrees
         header['BPA'] = 0.0
-        header['CUNIT1'] = 'DEG'
-        header['CUNIT2'] = 'DEG'
+        header['CUNIT1'] = 'deg'
+        header['CUNIT2'] = 'deg'
         header['CUNIT3'] = 'Hz'
         header['CUNIT4'] = ''
         
@@ -557,20 +556,18 @@ class TestFITSProcessing(unittest.TestCase):
         intensities = predict.image
         
         # Calculate expected scaling
-        bmaj_rad = np.deg2rad(header['BMAJ'])
-        bmin_rad = np.deg2rad(header['BMIN'])
-        pixel_area = np.abs(np.deg2rad(header['CDELT1'])) * np.abs(np.deg2rad(header['CDELT2']))
-        beam_area = (np.pi * bmaj_rad * bmin_rad) / (4 * np.log(2))
-        scale = 1.0 / (beam_area / pixel_area)
+        freq_scale = self.chan_freqs[0]/self.chan_freqs
+        bmaj = header['BMAJ'] * freq_scale
+        bmin = header['BMIN'] * freq_scale
+        # if image has multiple frequencies, then beam params need to scale with frequency
+        pixel_area = (self.cell_size/3600)**2
+        beam_area = (np.pi * bmaj* bmin) / (4 * np.log(2))
+        pixels_per_beam = beam_area / pixel_area
         
-        expected_intensities = np.zeros((self.img_size, self.img_size, self.chan_freqs.size, self.ncorr))
-        expected_intensities[self.img_size//2, self.img_size//2, :, :] = scale
-        expected_intensities = expected_intensities.reshape(self.img_size * self.img_size, self.chan_freqs.size, self.ncorr)
-        non_zero_mask = np.any(expected_intensities > self.tol, axis=(1, 2))
-        expected_intensities = expected_intensities[non_zero_mask]
-        
+        expected_intensities = np.ones((1, self.chan_freqs.size, self.ncorr))
+        expected_intensities /= pixels_per_beam[np.newaxis, :, np.newaxis]
         assert intensities.shape == expected_intensities.shape
-        assert np.allclose(intensities, expected_intensities)
+        assert np.allclose(intensities, expected_intensities, atol=1e-6)
         
 
     def test_bmaj1_bmin1_cube_scaling(self):
@@ -585,8 +582,8 @@ class TestFITSProcessing(unittest.TestCase):
 
         header = wcs.to_header()
         header['BUNIT'] = 'Jy/beam'
-        header['CUNIT1'] = 'DEG'
-        header['CUNIT2'] = 'DEG'
+        header['CUNIT1'] = 'deg'
+        header['CUNIT2'] = 'deg'
         header['CUNIT3'] = 'Hz'
         header['CUNIT4'] = ''
 
@@ -620,7 +617,7 @@ class TestFITSProcessing(unittest.TestCase):
         expected_intensities = expected_intensities[non_zero_mask]
         
         assert intensities.shape == expected_intensities.shape
-        assert np.allclose(intensities, expected_intensities)
+        assert np.allclose(intensities, expected_intensities, atol=1e-6)
         
 
     def test_beam_table_scaling(self):
@@ -635,8 +632,8 @@ class TestFITSProcessing(unittest.TestCase):
         
         header = wcs.to_header()
         header['BUNIT'] = 'Jy/beam'
-        header['CUNIT1'] = 'DEG'
-        header['CUNIT2'] = 'DEG'
+        header['CUNIT1'] = 'deg'
+        header['CUNIT2'] = 'deg'
         header['CUNIT3'] = 'Hz'
         header['CUNIT4'] = ''
 