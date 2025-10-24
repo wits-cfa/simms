87b59a5618af1c822437b0b2927b13c90768be82
Zaryn Abbu <2447140@students.wits.ac.za>
2025-10-24 17:17:53 +0000

    ruff checks long lines fixed

--- simms/skymodel/skymods.py
+++ simms/skymodel/skymods.py
@@ -153,9 +153,9 @@ def skymodel_from_fits(
         basis (str): polarisation basis ("linear" or "circular")
         tol (float): tolerance for pixel brightness
         stokes (Union[int,str]): Stokes parameter to use (0 = I, 1 = Q, 2 = U, 3 = V). If 'all',
-        all Stokes parameters are used.
+            all Stokes parameters are used.
         stack_axis (str|Dict): Stack FITS images along this axis if multiple input images given.
-        If Dict, then these should be options to 'fitstoolz.reader.FitsData.add_axis()'
+            If Dict, then these should be options to 'fitstoolz.reader.FitsData.add_axis()'
         interpolation (str): Interpolation along frequency if there's a grid mismatch between the FITS image and MS
     Returns:
         predict_image (np.ndarray): pixel-by-pixel brightness matrix for each channel and correlation
@@ -188,8 +188,8 @@ def skymodel_from_fits(
                 fds.add_axis(**dummy_stokes)
             else:
                 raise RuntimeError(
-                    f"Input skymodel FITS images cannot combined along the given axis '{stack_axis}'"
-                    f"because it doesn't exist in the input images"
+                    f"Input skymodel FITS images cannot be combined along the given axis "
+                    f"'{stack_axis}' because it doesn't exist in the input images."
                 )
 
         fds.expand_along_axis_from_files(stack_axis, input_fitsimages[1:])
@@ -256,8 +256,9 @@ def skymodel_from_fits(
         pass
     elif not is_range_in_range(ms_range, fits_range):
         raise SkymodelError(
-            f"MS frequencies [{ms_start_freq / 1e9:.6f} GHz, {ms_end_freq / 1e9:.6f} GHz] "
-            f"are outside the FITS image frequencies[{fits_start_freq / 1e9:.6f} GHz, {fits_end_freq / 1e9:.6f} GHz]. "
+            f"Some MS frequencies [{ms_start_freq / 1e9:.6f} GHz, {ms_end_freq / 1e9:.6f} GHz] "
+            f"are out of bounds of FITS image frequencies "
+            f"[{fits_start_freq / 1e9:.6f} GHz, {fits_end_freq / 1e9:.6f} GHz]. "
             "Cannot interpolate FITS image onto MS frequency grid."
         )
 
@@ -333,6 +334,8 @@ def skymodel_from_fits(
     if use_dft is None:
         if sparsity >= 0.8:
             log.info(
+                f"More than 80% of pixels have intensity < {(tol * 1e6):.2f} μJy. "
+                "DFT will be used for visibility prediction."
                 f"More than 80% of pixels have intensity < {(tol * 1e6):.2f} μJy. "
                 "DFT will be used for visibility prediction."
             )
--- simms_tests/fits_processing_tests.py
+++ simms_tests/fits_processing_tests.py
@@ -336,21 +336,22 @@ def test_full_stokes_fits_list_processing(params):
     assert intensities.shape == expected_intensities.shape
     assert np.allclose(intensities, expected_intensities)
 
-
-# TODO: Modify test below to check use of second element of Stokes axis
-# def test_stokes_axis_in_fits_processing(self):
-#     """
-#     Tests that the code raises an error when a FITS file contains a Stokes axis
-#     Validates:
-#         - error message
-#     """
-#     # create a FITS file with Stokes ndim > 1
-#     wcs = WCS(naxis=4)
-#     wcs.wcs.ctype = ['RA---SIN', 'DEC--SIN', 'FREQ', 'STOKES'
-#     wcs.wcs.cdelt = np.array([-params.cell_size/3600, params.cell_size/3600,
-#                                params.chan_freqs[1]-params.chan_freqs[0], 1.0])
-#     wcs.wcs.crpix = [params.img_size/2, params.img_size/2, 1, 1] # reference pixel
-#     wcs.wcs.crval = [0.0, -30.0, params.chan_freqs[0], 1] # reference pixel RA and Dec in deg
+    # TODO: Modify test below to check use of second element of Stokes axis
+    # def test_stokes_axis_in_fits_processing(self):
+    #     """
+    #     Tests that the code raises an error when a FITS file contains a Stokes axis
+    #     Validates:
+    #         - error message
+    #     """
+    #     # create a FITS file with Stokes ndim > 1
+    #     wcs = WCS(naxis=4)
+    #     wcs.wcs.ctype = ['RA---SIN', 'DEC--SIN', 'FREQ', 'STOKES']
+    #     wcs.wcs.cdelt = np.array([-params.cell_size/3600, 
+    #                                params.cell_size/3600,
+    #                                params.chan_freqs[1]-params.chan_freqs[0], 
+    #                                1.0]) # pixel scale in deg
+    #     wcs.wcs.crpix = [params.img_size/2, params.img_size/2, 1, 1] # reference pixel
+    #     wcs.wcs.crval = [0.0, -30.0, params.chan_freqs[0], 1] # reference pixel RA and Dec in deg
 
 #     # make header
 #     header = wcs.to_header()
@@ -365,25 +366,31 @@ def test_full_stokes_fits_list_processing(params):
 #     params.test_files.append(test_filename)
 #     hdu.writeto(test_filename, overwrite=True)
 
-#     # process the FITS file
-#     with params.assertRaises(SkymodelError):
-#         skymodel_from_fits(test_filename, 0.0, np.deg2rad(-30.0), params.chan_freqs, params.ms_delta_nu,
-#                            params.ncorr, params.basis)
+    #     # process the FITS file
+    #     with params.assertRaises(SkymodelError):
+    #         skymodel_from_fits(test_filename, 0.0, 
+    #                            np.deg2rad(-30.0), 
+    #                            params.chan_freqs, 
+    #                            params.ms_delta_nu, 
+    #                            params.ncorr, 
+    #                            params.basis)
 
-# TODO: Modify test below to check use of only first element of temporal axis
-# def test_time_axis_fits_processing(self):
-#     """
-#     Tests that the code raises an error when a FITS file contains a temporal axis
-#     Validates:
-#         - error message
-#     """
-#     # create a FITS file with time axis
-#     wcs = WCS(naxis=4)
-#     wcs.wcs.ctype = ['RA---SIN', 'DEC--SIN', 'FREQ', 'TIME']
-#     wcs.wcs.cdelt = np.array([-params.cell_size/3600, params.cell_size/3600,
-#                               params.chan_freqs[1]-params.chan_freqs[0], 1.0])
-#     wcs.wcs.crpix = [params.img_size/2, params.img_size/2, 1, 1] # reference pixel
-#     wcs.wcs.crval = [0.0, -30.0, params.chan_freqs[0], 1] # reference pixel RA and Dec in deg
+    # TODO: Modify test below to check use of only first element of temporal axis
+    # def test_time_axis_fits_processing(self):
+    #     """
+    #     Tests that the code raises an error when a FITS file contains a temporal axis
+    #     Validates:
+    #         - error message
+    #     """
+    #     # create a FITS file with time axis
+    #     wcs = WCS(naxis=4)
+    #     wcs.wcs.ctype = ['RA---SIN', 'DEC--SIN', 'FREQ', 'TIME']
+    #     wcs.wcs.cdelt = np.array([-params.cell_size/3600, 
+    #                                params.cell_size/3600, 
+    #                                params.chan_freqs[1]-params.chan_freqs[0], 
+    #                                1.0]) # pixel scale in deg
+    #     wcs.wcs.crpix = [params.img_size/2, params.img_size/2, 1, 1] # reference pixel
+    #     wcs.wcs.crval = [0.0, -30.0, params.chan_freqs[0], 1] # reference pixel RA and Dec in deg
 
 #     # make header
 #     header = wcs.to_header()
@@ -398,10 +405,14 @@ def test_full_stokes_fits_list_processing(params):
 #     params.test_files.append(test_filename)
 #     hdu.writeto(test_filename, overwrite=True)
 
-#     # process the FITS file
-#     with params.assertRaises(SkymodelError):
-#         skymodel_from_fits(test_filename, 0.0, np.deg2rad(-30.0), params.chan_freqs, params.ms_delta_nu,
-#                            params.ncorr, params.basis)
+    #     # process the FITS file
+    #     with params.assertRaises(SkymodelError):
+    #         skymodel_from_fits(test_filename, 0.0, 
+    #                            np.deg2rad(-30.0), 
+    #                            params.chan_freqs, 
+    #                            params.ms_delta_nu, 
+    #                            params.ncorr, 
+    #                            params.basis)
 
 # FIXME: Incorporate the two tests below into the ones above as lm-grid is no longer created
 # when FFT is used for visibility prediction.
@@ -433,9 +444,13 @@ def test_full_stokes_fits_list_processing(params):
 #     params.test_files.append(test_filename)
 #     hdu.writeto(test_filename, overwrite=True)
 
-#     # process the FITS file
-#     _, lm, _, _, _, _ = skymodel_from_fits(test_filename, 0.0, np.deg2rad(-30.0), params.chan_freqs,
-#                                            params.ms_delta_nu, params.ncorr, params.basis)
+    #     # process the FITS file
+    #     _, lm, _, _, _, _ = skymodel_from_fits(test_filename, 0.0, 
+    #                                            np.deg2rad(-30.0), 
+    #                                            params.chan_freqs, 
+    #                                            params.ms_delta_nu, 
+    #                                            params.ncorr, 
+    #                                            params.basis)
 
 #     # created expected l-m grid
 #     delt = np.deg2rad(params.cell_size/3600)
@@ -459,13 +474,14 @@ def test_full_stokes_fits_list_processing(params):
 #         - output l-m grid values
 #     """
 
-#     # create a FITS file with Stokes I only
-#     wcs = WCS(naxis=3)
-#     wcs.wcs.ctype = ['RA---SIN', 'DEC--SIN', 'FREQ']
-#     wcs.wcs.cdelt = np.array([-params.cell_size/3600, params.cell_size/3600,
-#                              params.chan_freqs[1]-params.chan_freqs[0]])
-#     wcs.wcs.crpix = [params.img_size/2, params.img_size/2, 1] # reference pixel
-#     wcs.wcs.crval = [0.0, -30.0, params.chan_freqs[0]] # reference pixel RA and Dec in deg
+    #     # create a FITS file with Stokes I only
+    #     wcs = WCS(naxis=3)
+    #     wcs.wcs.ctype = ['RA---SIN', 'DEC--SIN', 'FREQ']
+    #     wcs.wcs.cdelt = np.array([-params.cell_size/3600, 
+    #                                params.cell_size/3600, p
+    #                                params.chan_freqs[1]-params.chan_freqs[0]]) # pixel scale in deg
+    #     wcs.wcs.crpix = [params.img_size/2, params.img_size/2, 1] # reference pixel
+    #     wcs.wcs.crval = [0.0, -30.0, params.chan_freqs[0]] # reference pixel RA and Dec in deg
 
 #     # make header
 #     header = wcs.to_header()
@@ -480,9 +496,13 @@ def test_full_stokes_fits_list_processing(params):
 #     params.test_files.append(test_filename)
 #     hdu.writeto(test_filename, overwrite=True)
 
-#     # process the FITS file
-#     _, lm, _, _, _, _ = skymodel_from_fits(test_filename, 0.0, np.deg2rad(-30.0), params.chan_freqs,
-#                                            params.ms_delta_nu, params.ncorr, params.basis)
+    #     # process the FITS file
+    #     _, lm, _, _, _, _ = skymodel_from_fits(test_filename, 0.0, 
+    #                                            np.deg2rad(-30.0), 
+    #                                            params.chan_freqs, 
+    #                                            params.ms_delta_nu, 
+    #                                            params.ncorr, 
+    #                                            params.basis)
 
 #     # created expected l-m grid
 #     delt = np.deg2rad(params.cell_size/3600)
