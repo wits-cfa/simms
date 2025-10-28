62384385e7e2467c19b31c42b5a9a6dde66f3f4d
sphemakh <sphemakh@gmail.com>
2025-10-28 22:38:14 +0200

    add command chain tests

--- simms_tests/telsim_ci_tests.py
+++ simms_tests/telsim_ci_tests.py
@@ -2,7 +2,7 @@ import subprocess
 
 import pytest
 
-from . import InitTest
+from . import TESTDIR, InitTest
 
 
 @pytest.fixture
@@ -34,6 +34,54 @@ def test_telsim_subarray(params):
             params.random_named_directory(suffix=".ms"),
         ]
     )
+
+
+def test_telsim_skysim_chain(params):
+    main_args = [
+        "simms",
+        "--chain",
+        "--ms",
+        params.random_named_directory(suffix=".ms"),
+    ]
+
+    telsim_args = [
+        "telsim",
+        "--telescope",
+        "kat-7",
+    ]
+
+    simms_args = [
+        "skysim",
+        "--ascii-sky",
+        f"{TESTDIR}/testsky.txt",
+    ]
+
+    subprocess.check_call(main_args + telsim_args + simms_args)
+
+
+def test_error_telsim_chain_double_ms_spec(params):
+    ms = params.random_named_directory(suffix=".ms")
+    main_args = [
+        "simms",
+        "--chain",
+        "--ms",
+        ms,
+    ]
+
+    telsim_args = [
+        "telsim",
+        "--telescope",
+        "kat-7",
+    ]
+
+    # this should work
+    subprocess.check_output(main_args + telsim_args)
+
+    # now it should fail because the MS is being specified when chained is true
+    with pytest.raises(subprocess.CalledProcessError) as exception:
+        subprocess.check_call(main_args + telsim_args + [ms])
+
+    assert exception.type is subprocess.CalledProcessError
 
 
 # TODO(mukundi,galefang)
